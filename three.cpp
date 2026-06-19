7611880e4191aa202c14f39d75f5423c083519c5
Vaibhav Gera <vaibhavgera3913@gmail.com>
2026-06-19 23:23:59 +0530

    null pointer

---
+++ three.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    // lernig of null pointer
+    int *ptr=NULL;
+
+    int ** prpnt;
+    cout<< ptr<<endl;
+    cout<< prpnt<<endl;    //this will give some garbage value
+    
+return 0;
+}
